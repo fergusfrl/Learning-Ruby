bb48bda78365c2bd2b5b8f059c9cc3f0999f2f1c
Fergus Farrell <fergusfrl@gmail.com>
2017-06-24 10:50:04 +1200

    Update MonkeysAndCoconuts.rb
    Added comments explaining problem

--- MonkeysAndCoconuts.rb
+++ MonkeysAndCoconuts.rb
@@ -1,1 +1,10 @@
+#Monkeys And Coconuts
+#Fergus Farrell - 23/06/2017
 
+#'n' number of men have washed up on the shore of a deserted island.
+#They gather all the coconuts on the island and stack them in the middle.
+#The men do not trust each other so in the night, 1 by 1, each man:
+#   Splits the coconut pile into n equal piles, there is always 1 coconut left over (which he gives to the monkey)
+#   The man then hides 1 pile and puts remaining piles together to reform the large pile
+
+#Question: How many coconuts did the men start with?
