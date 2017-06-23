781f978ff46fa49e8af51dd9b2ebc8399eec108c
Fergus Farrell <fergusfrl@gmail.com>
2017-06-24 11:18:03 +1200

    Update MonkeysAndCoconuts.rb
    Iterates through possible last man piles. Adds coconuts to the last pile to find starting number of coconuts.
    Currently not adding correctly, doubling all. Good for 2nd gen, 3rd, 4th etc. should not be doubled.

--- MonkeysAndCoconuts.rb
+++ MonkeysAndCoconuts.rb
@@ -7,4 +7,27 @@
 #   Splits the coconut pile into n equal piles, there is always 1 coconut left over (which he gives to the monkey)
 #   The man then hides 1 pile and puts remaining piles together to reform the large pile
 
-#Question: How many coconuts did the men start with?
+#Question: What is the minimum number of coconuts the men could start with?
+
+def main(men = 5)
+  @men = men
+  i = 0
+  while true do
+    if i >= 10
+      break
+    end
+    splitPile(i)
+    i = i+1
+  end
+end
+
+def splitPile(n = 0)
+  i = 0
+  while i < @men
+    n = n * 2 + 1
+    i = i + 1
+  end
+  puts n
+end
+
+main
