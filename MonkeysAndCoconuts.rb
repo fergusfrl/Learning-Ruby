0b44ea8473789561dad611b35da40f10a1f9b252
Fergus Farrell <fergusfrl@gmail.com>
2017-06-24 12:25:53 +1200

    Update MonkeysAndCoconuts.rb
    Recursive approach. Currently crashes :0 oops

--- MonkeysAndCoconuts.rb
+++ MonkeysAndCoconuts.rb
@@ -4,30 +4,33 @@
 #'n' number of men have washed up on the shore of a deserted island.
 #They gather all the coconuts on the island and stack them in the middle.
 #The men do not trust each other so in the night, 1 by 1, each man:
-#   Splits the coconut pile into n equal piles, there is always 1 coconut left over (which he gives to the monkey)
-#   The man then hides 1 pile and puts remaining piles together to reform the large pile
+#   - Splits the coconut pile into n equal piles, there is always 1 coconut left over (which he gives to the monkey)
+#   - The man then hides 1 pile and puts remaining piles together to reform the large pile
+#The next day, the pile divides perfectly between all men
 
 #Question: What is the minimum number of coconuts the men could start with?
 
+def checkRecursion(n, men)
+  if men == 1
+    return true
+  end
+  if (n-1)%men != 0
+    return false 
+  end
+  
+  n = n - ((n-1)/(men*1.0))
+  puts n
+  checkRecursion(n, men-1)
+end
+
 def main(men = 5)
-  @men = men
   i = 0
-  while true do
-    if i >= 10
-      break
-    end
-    splitPile(i)
+  found = false
+  while !found do
+    found = checkRecursion(i, men)
     i = i+1
   end
+  puts "First Result = #{i}"
 end
 
-def splitPile(n = 0)
-  i = 0
-  while i < @men
-    n = n * 2 + 1
-    i = i + 1
-  end
-  puts n
-end
-
-main
+main()
