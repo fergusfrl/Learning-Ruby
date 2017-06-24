d9679251647d59db3365ff1e657160950f1636ca
Fergus Farrell <fergusfrl@gmail.com>
2017-06-24 14:38:29 +1200

    Create SIRModel.rb
    Added explanation of program

---
+++ SIRModel.rb
@@ -0,0 +1,8 @@
+#Susceptible, Infected, Recovered Model
+#Fergus Farrell - 24/06/2017
+
+#Models the spread of a disease through a population.
+#Individuals can be S - succepable, I - infected or R - recovered
+#Input: SizeOfPopulation, InitialNoInfected, S->I rate, I->R rate, S->R rate
+#Input Example: 10000, 10, 0.01, 0.01, 0.015
+
